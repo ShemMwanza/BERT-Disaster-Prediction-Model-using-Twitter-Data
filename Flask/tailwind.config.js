7c656461ce7b693875628cccee2b0fd878231bac
Shem Mwanza <shem.mwanza@strathmore.edu>
2022-10-26 23:32:08 +0300

    Flask and Tailwind

---
+++ Flask/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./js/styles/**/*.js",
+    "./js/components/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
